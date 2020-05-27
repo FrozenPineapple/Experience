7e77805ee08d0514ffc1990ff781ded2e6ad9c2c
FrozenPineapple <65616448+FrozenPineapple@users.noreply.github.com>
2020-05-27 12:31:57 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+//let xposition = 0;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255, 100, 80);
+  //background(255);
+}
+
+function draw() {
+
+
+  fill(30, 200, 100);
+  circle(mouseX, mouseY, 50);
+
+  //xposition = xposition + 5;
+
+
+
+
+
+}
